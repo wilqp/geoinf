17143793f8a42432f63a4eb814cc2c8d7c7af771
wilqp <wwilqp+github@gmail.com>
2015-01-19 17:19:36 +0000

    sprawne

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('myApp',[])
+
+    .controller('myCtrl',function($scope, $http){
+        var url= 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK'    
+        
+        $http.jsonp(url)
+            .success(function(data){
+                $scope.results = data.feed.entry;
+            });
+    });
